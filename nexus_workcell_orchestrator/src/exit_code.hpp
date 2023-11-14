224bd24dc6aa55cb6ededda8bbd94caae155a2ed
Teo Koon Peng <koonpeng@openrobotics.org>
2023-11-14 16:09:05 +0800

    remove exceptions
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

---
+++ nexus_workcell_orchestrator/src/exit_code.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef NEXUS_WORKCELL_ORCHESTRATOR__EXIT_CODE_HPP
+#define NEXUS_WORKCELL_ORCHESTRATOR__EXIT_CODE_HPP
+
+namespace nexus::workcell_orchestrator {
+
+constexpr int EXIT_CODE_INVALID_PARAMETER = 1;
+constexpr int EXIT_CODE_REGISTRATION_FAILED = 2;
+
+}
+
+#endif
