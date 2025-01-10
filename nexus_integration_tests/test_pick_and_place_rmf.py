161b2a4cfff7d0f91624bc05dcdf94260bcb7c66
Luca Della Vedova <lucadv@intrinsic.ai>
2025-01-10 16:03:28 +0800

    Vendor RMF launch files, remove building_map_tools
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- nexus_integration_tests/test_pick_and_place_rmf.py
+++ nexus_integration_tests/test_pick_and_place_rmf.py
@@ -40,7 +40,7 @@ class PickAndPlaceTest(NexusTestCase):
         subprocess.Popen('pkill -9 -f zenoh', shell=True)
 
         self.proc = managed_process(
-            ("ros2", "launch", "nexus_integration_tests", "office.launch.xml", "sim_update_rate:=10000", "main_bt_filename:=main_rmf.xml"),
+            ("ros2", "launch", "nexus_integration_tests", "depot.launch.xml", "sim_update_rate:=10000", "main_bt_filename:=main_rmf.xml"),
         )
         self.proc.__enter__()
         print("waiting for nodes to be ready...", file=sys.stderr)
