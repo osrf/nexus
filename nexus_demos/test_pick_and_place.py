05c4bbdb027c935ab6f0cf523449b35f123dfb3f
Aaron Chong <aaronchongth@gmail.com>
2025-02-04 18:00:20 +0800

    Correct tests
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- nexus_demos/test_pick_and_place.py
+++ nexus_demos/test_pick_and_place.py
@@ -67,7 +67,6 @@ class PickAndPlaceTest(NexusTestCase):
     async def test_pick_and_place_wo(self):
         self.action_client.wait_for_server()
         goal_msg = ExecuteWorkOrder.Goal()
-        goal_msg.order.id = 'test_pick_and_place'
         with open(f"{os.path.dirname(__file__)}/config/pick_from_conveyor.json") as f:
             goal_msg.order.work_order = f.read()
         feedbacks: list[ExecuteWorkOrder.Feedback] = []
@@ -94,7 +93,7 @@ class PickAndPlaceTest(NexusTestCase):
             self.assertEqual(len(msg.task_states), 1)
             state: TaskState = msg.task_states[0]  # type: ignore
             self.assertEqual(state.workcell_id, "workcell_2")
-            self.assertEqual(state.task_id, "test_pick_and_place/pick_from_conveyor")
+            self.assertEqual(state.task_id, "test-1/pick_from_conveyor")
 
         state: TaskState = feedbacks[-1].task_states[0]  # type: ignore
         self.assertEqual(state.status, TaskState.STATUS_FINISHED)
