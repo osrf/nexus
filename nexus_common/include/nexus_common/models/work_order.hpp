1bd70741c3c97de597dac5ce3511093df0ad89e3
Aaron Chong <aaronchongth@gmail.com>
2025-04-21 18:52:32 +0800

    Re-vert WorkOrder to class
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- nexus_common/include/nexus_common/models/work_order.hpp
+++ nexus_common/include/nexus_common/models/work_order.hpp
@@ -26,7 +26,7 @@
 
 namespace nexus::common {
 
-struct WorkOrder
+class WorkOrder
 {
 public: struct Step
   {
@@ -73,9 +73,9 @@ public: YAML::Node yaml;
 public: WorkOrder(YAML::Node yaml)
   : yaml(std::move(yaml)) {}
 
-  WorkOrder() {}
+public: WorkOrder() {}
 
-  std::string work_instruction_name() const
+public: std::string work_instruction_name() const
   {
     return this->yaml["workInstructionName"].as<std::string>();
   }
